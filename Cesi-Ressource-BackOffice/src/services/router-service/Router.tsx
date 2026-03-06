0e264617bd3c42ac476b0d3387e8e2a552ae6d41
hugo <hcaudron1@gmail.com>
2026-03-06 15:59:13 +0100

    feat : add pages to router

--- Cesi-Ressource-BackOffice/src/services/router-service/Router.tsx
+++ Cesi-Ressource-BackOffice/src/services/router-service/Router.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from '../../pages/login';
-import Detail from '../../pages/detail';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../../pages/login";
+import Detail from "../../pages/detail";
+import { HeaderLayout } from "./HeaderLayout";
+import ForgotPassword from "../../pages/forgotPassword";
 
 const Router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>(
-					<>
-						<Route path="/" element={<Login />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/detail" element={<Detail />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        (
+        <>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+		  <Route path="/forgot-password" element={<ForgotPassword />} />
 
-					</>
-				)
-			</Routes>
-		</BrowserRouter>
-	);
+          <Route path="/detail" element={<HeaderLayout />}>
+            <Route index element={<Detail />} />
+          </Route>
+        </>
+        )
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
